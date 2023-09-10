61d4d47336d8d410468ebe00580ec03479a8ac0c
Tracy Chesnut <tlchesnut.az@gmail.com>
2023-09-10 11:36:55 -0600

    performance fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom"
 
 
 export default function Home() {
-    return(
-         
-        <div className="container">
-            <h1 className="title"> RECREATION: </h1> 
-            <h2 className="subtitle"> what does it mean to you? </h2>
-            <p>our National Parks are just the beginning</p>
-            <p>explore some of 'the other' recreation areas</p> 
-            <p>and find your next favorite 'happy place'</p>
-            <Link to="/search">Search Recreation Areas</Link>
-        </div>
+	return(
+				
+		<div className="container">
+			<h1 className="title"> RECREATION: </h1> 
+			<h2 className="subtitle"> what does it mean to you? </h2>
+			<p>our National Parks are just the beginning</p>
+			<p>explore some of 'the other' recreation areas</p> 
+			<p>and find your next favorite 'happy place'</p>
+			<Link to="/search">Search Recreation Areas</Link>
+		</div>
 
-    )
+	)
 }
